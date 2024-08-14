23cdb6bfccac022d30f0f1a591e466ad561fb804
ozcnii <ercount89@gmail.com>
2024-08-14 07:01:49 +0400

    fix: proportions

--- src/modules/earn-game/components/GameArea.tsx
+++ src/modules/earn-game/components/GameArea.tsx
@@ -4,8 +4,18 @@ import { Spinner } from "@telegram-apps/telegram-ui";
 import { useLayoutEffect, useRef, useState } from "react";
 
 const SLIDES_STEP_PX = 10;
+
 const ORIGINAL_MAIN_BG_HEIGHT = 3840;
-const ORIGINAL_TREE_POSITION_Y = 3190;
+const ORIGINAL_TREE_POSITION_Y = 3190 + 25;
+
+const ORIGINAL_CONTENT_WIDTH = 428;
+const ORIGINAL_TREE_WIDTH = 90;
+const ORIGINAL_TREE_HEIGHT = 128.2;
+const ORIGINAL_TREE_BASE_WIDTH = 142;
+const ORIGINAL_TREE_BASE_HEIGHT = 143;
+
+const ORIGINAL_TREE_CROWN_WIDTH = 318;
+const ORIGINAL_TREE_CROWN_HEIGHT = 388;
 
 export const GameArea = ({
   decPoints,
@@ -49,14 +59,41 @@ export const GameArea = ({
       return;
     }
 
-    // TODO: верхушку дерева также посчитать
-    document.documentElement.style.setProperty("--tree-width", "90px");
-    document.documentElement.style.setProperty("--tree-base-width", "142px");
+    const contentWidth = mainBgRef.current.clientWidth;
+    const treeScale = contentWidth / ORIGINAL_CONTENT_WIDTH;
+
+    const newTreeChunkWidth = treeScale * ORIGINAL_TREE_WIDTH;
+    const newTreeChunkHeight = treeScale * ORIGINAL_TREE_HEIGHT;
+    const newTreeBaseWidth = treeScale * ORIGINAL_TREE_BASE_WIDTH;
+    const newTreeBaseHeight = treeScale * ORIGINAL_TREE_BASE_HEIGHT;
+    const newTreeCrownWidth = treeScale * ORIGINAL_TREE_CROWN_WIDTH;
+    const newTreeCrownHeight = treeScale * ORIGINAL_TREE_CROWN_HEIGHT;
+
+    document.documentElement.style.setProperty(
+      "--tree-width",
+      newTreeChunkWidth + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--tree-base-width",
+      newTreeBaseWidth + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--tree-base-height",
+      newTreeBaseHeight + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--tree-crown-width",
+      newTreeCrownWidth + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--tree-crown-height",
+      newTreeCrownHeight + "px"
+    );
 
     const currentImageHeight = mainBgRef.current.clientHeight;
-    const scale = currentImageHeight / ORIGINAL_MAIN_BG_HEIGHT;
-    const yPos = ORIGINAL_TREE_POSITION_Y * scale;
-    const initialTreeBottomOffset = currentImageHeight - yPos;
+    const bgScale = currentImageHeight / ORIGINAL_MAIN_BG_HEIGHT;
+    const initialTreeYPosition = ORIGINAL_TREE_POSITION_Y * bgScale;
+    const initialTreeBottomOffset = currentImageHeight - initialTreeYPosition;
 
     const slides = backgroundSlidesWrapperRef.current.children;
 
@@ -77,7 +114,12 @@ export const GameArea = ({
     treeContainerRef.current.style.bottom =
       currentSlidesTranslateY.current + initialTreeBottomOffset + "px";
 
-    treeTrunkRef.current.style.height = clicksToWin * SLIDES_STEP_PX + "px";
+    const potentialTreeHeight = clicksToWin * SLIDES_STEP_PX;
+    const realTreeHeight =
+      Math.ceil(potentialTreeHeight / newTreeChunkHeight) * newTreeChunkHeight;
+
+    treeTrunkRef.current.style.height = realTreeHeight + "px";
+
     treeTrunkRef.current.style.backgroundPositionY =
       treeTrunkRef.current.style.height;
 
@@ -175,6 +217,12 @@ export const GameArea = ({
         </div>
 
         <div ref={backgroundSlidesWrapperRef} className="bg-slides">
+          <div className="slide slide-3"></div>
+          <div className="slide slide-2"></div>
+          <div className="slide slide-3"></div>
+          <div className="slide slide-2"></div>
+          <div className="slide slide-3"></div>
+          <div className="slide slide-2"></div>
           <div className="slide slide-3"></div>
           <div className="slide slide-2"></div>
           <div className="slide slide-3"></div>
